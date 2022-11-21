03f73810fa8efb64a4224a9430f21c822610dc90
Kaiyu Chen <greatcky83@gmail.com>
2022-11-21 04:36:40 +0000

    Base tests - for every list pages.(Movies and People)

--- cypress/e2e/base.cy.js
+++ cypress/e2e/base.cy.js
@@ -1,5 +1,7 @@
-let movies; // List of movies from TMDB
-let movie; //
+let discoverMovies;
+let upComingMovies;
+let topRatedMovies;
+let popularityPeople;
 
 describe("Base tests", () => {
   before(() => {
@@ -11,9 +13,41 @@ describe("Base tests", () => {
     )
       .its("body") // Take the body of HTTP response from TMDB
       .then((response) => {
-        movies = response.results;
+        discoverMovies = response.results;
       });
+
+    cy.request(
+      `https://api.themoviedb.org/3/movie/upcoming?api_key=${Cypress.env(
+      "TMDB_KEY"
+      )}&include_adult=false&language=en-US&page=1`
+    )
+      .its("body")
+      .then((response) => {
+        upComingMovies = response.results;
+      });
+
+    cy.request(
+      `https://api.themoviedb.org/3/movie/top_rated?api_key=${Cypress.env(
+      "TMDB_KEY"
+      )}&language=en-US&page=1`
+    )
+      .its("body")
+      .then((response) => {
+        topRatedMovies = response.results;
+      });
+
+    cy.request(
+      `https://api.themoviedb.org/3/person/popular?api_key=${Cypress.env(
+          "TMDB_KEY"
+      )}&language=en-US&page=1`
+    )
+      .its("body")
+      .then((response) => {
+        popularityPeople = response.results;
+      });
+
   });
+
   beforeEach(() => {
     cy.visit("/");
   });
@@ -26,38 +60,71 @@ describe("Base tests", () => {
 
     it("displays the correct movie titles", () => {
       cy.get(".MuiCardHeader-content").each(($card, index) => {
-        cy.wrap($card).find("p").contains(movies[index].title);
+        cy.wrap($card).find("p").contains(discoverMovies[index].title);
       });
     });
   });
-  describe("The movie details page", () => {
-    before(() => {
-      cy.request(
-        `https://api.themoviedb.org/3/movie/${
-          movies[1].id
-        }?api_key=${Cypress.env("TMDB_KEY")}`
-      )
-        .its("body")
-        .then((movieDetails) => {
-          movie = movieDetails;
-        });
-    });
+
+  describe("The UpComing Movies page", () => {
     beforeEach(() => {
-      cy.visit(`/movies/${movies[1].id}`);
+      cy.get("button").contains("Movies").click();
+      cy.get("li").contains("Upcoming").click();
     });
-    it(" displays the movie title, overview and genres and ", () => {
-      cy.get("h3").contains(movie.title);
-      cy.get(".MuiGrid-root>.MuiTypography-h5").contains("Overview");
-      cy.get(".MuiGrid-root>.MuiTypography-h5").next().contains(movie.overview);
-      cy.get("ul")
-        .eq(1)
-        .within(() => {
-          const genreChipLabels = movie.genres.map((g) => g.name);
-          genreChipLabels.unshift("Genres");
-          cy.get("span").each(($card, index) => {
-            cy.wrap($card).contains(genreChipLabels[index]);
-          });
-        });
+    it("displays the page header and 20 movies", () => {
+      cy.get("h3").contains("Upcoming Movies");
+      cy.get(".MuiCardHeader-root").should("have.length", 20);
+    });
+
+    it("displays the correct movie titles", () => {
+      cy.get(".MuiCardHeader-content").each(($card, index) => {
+        cy.wrap($card).find("p").contains(upComingMovies[index].title);
+      });
+    });
+  });
+
+  describe("The Top Rated Movies page", () => {
+    beforeEach(() => {
+      cy.get("button").contains("Movies").click();
+      cy.get("li").contains("Top Rated").click();
+    });
+    it("displays the page header and 20 movies", () => {
+      cy.get("h3").contains("Top Rated Movies");
+      cy.get(".MuiCardHeader-root").should("have.length", 20);
+    });
+
+    it("displays the correct movie titles", () => {
+      cy.get(".MuiCardHeader-content").each(($card, index) => {
+        cy.wrap($card).find("p").contains(topRatedMovies[index].title);
+      });
+    });
+  });
+
+  describe("The Favorites page", () => {
+    beforeEach(() => {
+      cy.get("button").contains("Movies").click();
+      cy.get("li").contains("Favorites").click();
+    });
+    it("displays the page header and 0 movie", () => {
+      cy.get("h3").contains("Favorite Movies");
+      cy.get(".MuiCardHeader-root").should("have.length", 0);
+    });
+  });
+
+  describe("The Popular People page", () => {
+    beforeEach(() => {
+      cy.get("button").contains("People").click();
+    });
+    it("displays the page header and 20 persons", () => {
+      cy.get(".MuiGrid-grid-sm-6 .MuiCardHeader-content").should("have.length", 20);
+    });
+
+    it("displays the correct person names and popularity", () => {
+      cy.get(".MuiGrid-grid-sm-6 .MuiCardHeader-content").each(($card, index) => {
+        cy.wrap($card).find("p").contains(popularityPeople[index].name);
+      });
+      cy.get(".MuiGrid-grid-xs-6 p").each(($card, index) => {
+        cy.wrap($card).contains(popularityPeople[index].popularity);
+      });
     });
   });
 });
