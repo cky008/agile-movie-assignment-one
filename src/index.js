0a5180839a43769c4f557f125aa04a4b4fb932c6
Kaiyu Chen <greatcky83@gmail.com>
2022-11-22 03:09:55 +0000

    Add code splitting to Home Page.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React, { lazy, Suspense } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Navigate, Routes} from "react-router-dom";
-import HomePage from "./pages/homePage";
 import UpcomingMoviesPage from'./pages/upComingMoviesPage'
 import { QueryClientProvider, QueryClient } from "react-query";
 import { ReactQueryDevtools } from 'react-query/devtools'
@@ -17,6 +16,7 @@ const MovieReviewPage = lazy(() => import("./pages/movieReviewPage"));
 const AddMovieReviewPage = lazy(() => import("./pages/addMovieReviewPage"));
 const SiteHeader = lazy(() => import("./components/siteHeader"));
 const FavoriteMoviesPage = lazy(() => import("./pages/favoriteMoviesPage"));
+const HomePage = lazy(() => import("./pages/homePage"));
 
 
 
