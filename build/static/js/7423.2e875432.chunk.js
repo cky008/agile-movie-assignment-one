5ef498cfa4571b1c3238cfef5520ed45d23bde44
Kaiyu Chen <greatcky83@gmail.com>
2022-11-22 05:09:05 +0000

    Try to fix the bug caused by deployment

---
+++ build/static/js/7423.2e875432.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmovies=self.webpackChunkmovies||[]).push([[7423],{7423:function(n,e,s){s.r(e);var i=s(2791),r=s(5953),c=s(184),l=(0,i.lazy)((function(){return Promise.all([s.e(9411),s.e(593)]).then(s.bind(s,6051))}));e.default=function(n){var e=n.persons;return(0,c.jsx)(r.ZP,{container:!0,sx:{padding:"20px"},children:(0,c.jsx)(r.ZP,{item:!0,container:!0,spacing:5,children:(0,c.jsx)(i.Suspense,{fallback:(0,c.jsx)("h1",{children:"Building list"}),children:(0,c.jsx)(l,{persons:e})})})})}}}]);
+//# sourceMappingURL=7423.2e875432.chunk.js.map
